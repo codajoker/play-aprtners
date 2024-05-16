81b819789adff0622681df513207c34ed79e424e
codajoker <84983434+codajoker@users.noreply.github.com>
2024-05-16 11:37:39 +0200

    123

--- js/swiperTestimonials.js
+++ js/swiperTestimonials.js
@@ -1,24 +1,26 @@
 const options = {
-  autoplay: true,
+  autoplay: {
+    delay: 5000,
+  },
   initialSlide: 1,
-   effect: "coverflow",
-    centeredSlides: true,
-    centeredSlidesBounds: true,
-    centerInsufficientSlides: true,
-    slidesPerView: 1, 
-    slidesPerColumn: 1,
-    initialSlide: 1,
-    loop: true,
-    spaceBetween: 8,
-    breakpoints: {
-      1200: {
-        slidesPerView: 3,
-        spaceBetween: 40,
-      },
+  effect: "coverflow",
+  centeredSlides: true,
+  centeredSlidesBounds: true,
+  centerInsufficientSlides: true,
+  slidesPerView: 1,
+  slidesPerColumn: 1,
+  initialSlide: 1,
+  loop: true,
+  spaceBetween: 8,
+  breakpoints: {
+    1200: {
+      slidesPerView: 3,
+      spaceBetween: 40,
     },
-    navigation: {
-        nextEl: ".testimonials-next",
-        prevEl: ".testimonials-prev",
-    },
+  },
+  navigation: {
+    nextEl: ".testimonials-next",
+    prevEl: ".testimonials-prev",
+  },
 };
-const swiper = new Swiper('.js-swiper-testimonials', options);
+const swiper = new Swiper(".js-swiper-testimonials", options);
